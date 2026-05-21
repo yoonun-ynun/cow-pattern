e16d76fee6a5a6db21077c56743ac3e952f1762b
yoonun <yoontkyoonun@gmail.com>
2026-05-21 18:01:47 +0900

    feat: add docstring

--- compare/compare.py
+++ compare/compare.py
@@ -1,8 +1,8 @@
 class Compare:
-    def check(self, vector: list[float]) -> bool:
+    def check(self, vector: list[float]) -> int | None:
         """
         DB에 해당 우비가 있는지 검사합니다.
         :param vector: 검사할 소의 특징 백터입니다.
-        :return: 있을경우 true, 없을경우 false를 반환합니다.
+        :return: 있을경우 소의 고유id, 없을경우 None을 반환합니다.
         """
         pass
